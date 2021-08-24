730dae721a6759e2677fa1c60890bd73c9ffb5a7
宋锦伟 <songjinwei@qianxin.com>
2021-08-24 10:14:26 +0800

    add lsp config

--- lua/plugin-config/lsp.lua
+++ lua/plugin-config/lsp.lua
@@ -73,3 +73,29 @@ lspSymbol("Information", "")
 lspSymbol("Hint", "")
 
 -- vim.lsp.set_log_level("debug")
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+    vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    {
+        virtual_text = {
+            prefix = "",
+            spacing = 0
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = false -- update diagnostics insert mode
+    }
+)
+
+-- suppress error messages from lang servers
+vim.notify = function(msg, log_level, _opts)
+    if msg:match "exit code" then
+        return
+    end
+    if log_level == vim.log.levels.ERROR then
+        vim.api.nvim_err_writeln(msg)
+    else
+        vim.api.nvim_echo({{msg}}, true, {})
+    end
+end
