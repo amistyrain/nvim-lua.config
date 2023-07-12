41864e71455258447479249ee5aee8374a607f30
amistyrain <mrsong.jinwei@gmail.com>
2023-07-12 21:55:39 +0800

    add markdown and fix whichkey

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -258,6 +258,11 @@ return {
             vim.o.timeout = true
             vim.o.timeoutlen = 300
         end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
     },
 
     {
@@ -355,5 +360,22 @@ return {
         init = function()
             require("plugin-map").diffview()
         end
-    }
+    },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app && npm install",
+        ft = "markdown",
+        lazy = true,
+        keys = { { "gm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" } },
+        config = function()
+            vim.g.mkdp_auto_close = true
+            vim.g.mkdp_open_to_the_world = false
+            vim.g.mkdp_open_ip = "127.0.0.1"
+            vim.g.mkdp_port = "8888"
+            vim.g.mkdp_browser = ""
+            vim.g.mkdp_echo_preview_url = true
+            vim.g.mkdp_page_title = "${name}"
+        end,
+    },
 }
